4a21533f85e06971a186a139372aef0effd553b5
Marius <Marius>
2019-11-04 12:57:48 +0100

    JO

--- Beispielprojekt/Tank.cpp
+++ Beispielprojekt/Tank.cpp
@@ -12,14 +12,19 @@
 // Simulationsgeschwindigkeit
 const double DT = 100.0;
 
+double x = 0;
+double y = 0;
+double press = 0;
+
 class GameWindow : public Gosu::Window
 {
 public:
-	Gosu::Image bild;
+	Gosu::Image panzer;
 	GameWindow()
 		: Window(800, 600)
+		, panzer("Panzer.png")
 	{
-		set_caption("TEST2");
+		set_caption("PANZAAA");
 	}
 
 	// wird bis zu 60x pro Sekunde aufgerufen.
@@ -27,11 +32,28 @@ public:
 	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
 	void draw() override
 	{
+		panzer.draw_rot(x, y, 0.0,
+			press, // Rotationswinkel in Grad
+			0.5, 0.5 // Position der "Mitte" relativ zu x, y
+		);
+
 	}
 
 	// Wird 60x pro Sekunde aufgerufen
 	void update() override
 	{
+		x = input().mouse_x();
+		y = input().mouse_y();
+
+
+
+		if (input().down(Gosu::KB_LEFT)) {
+			press += 10;
+		}
+		else if (input().down(Gosu::KB_RIGHT)) {
+			press -= 10;
+		}
+
 	}
 };
 
