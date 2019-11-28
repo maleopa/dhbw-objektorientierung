9796bde473854144cba3e58f7df37c7105b6022f
Marius <Marius>
2019-11-28 16:05:30 +0100

    Schuss

--- Beispielprojekt/Tank.cpp
+++ Beispielprojekt/Tank.cpp
@@ -15,7 +15,6 @@ using namespace std;
 
 class panzer {
 private:
-		
 	
 	double winkel = 0;
 	double x;
@@ -23,12 +22,10 @@ private:
 	double v;
 	Gosu::Image panzerbild;
 
-	
-
 public:
 	
 
-	panzer(double xa, double ya, double wa, double va) : x(xa), y(ya), winkel(wa),v(va), panzerbild("panzer.png") {}
+	panzer(double xa, double ya, double wa, double va) : x(xa), y(ya), winkel(wa),v(va), panzerbild("tank2.png") {}
 
 	
 	void move() {
@@ -71,6 +68,46 @@ public:
 
 };
 
+class rakete {
+private:
+
+	double winkel = 0;
+	double x;
+	double y;
+	double v;
+	Gosu::Image raketenbild;
+
+public:
+
+
+	rakete(double xa, double ya, double wa, double va) : x(xa), y(ya), winkel(wa), v(va), raketenbild("rakete2.png") {}
+
+	void move() {
+		this->x = this->x + this->v * cos((this->winkel - 90) / 180 * 3.14);
+		this->y = this->y + this->v * sin((this->winkel - 90) / 180 * 3.14);
+	}
+
+	void set_v(double v) {
+		this->v = v;
+	}
+	void set_x(double x) {
+		this->x = x;
+	}
+	void set_y(double y) {
+		this->y = y;
+	}
+	
+	void set_winkel(double w){
+		this->winkel = w;
+	}
+
+	void draw_rakete() {
+		raketenbild.draw_rot(this->x, this->y, 0.4, this->winkel, 0.5, 1.0);
+	}
+
+};
+
+
 class hintergrund {
 private:
 	double x=0;
@@ -111,6 +148,19 @@ void rechts_links(panzer* p) {
 	}
 }
 
+void abschuss_move(rakete* r, panzer* p) {
+
+	if (Gosu::Input::down(Gosu::KB_SPACE)) {
+		r->set_v(5);
+		r->set_winkel(p->get_winkel());
+		r->set_x(p->get_x());
+		r->set_y(p->get_y());
+	}
+	
+}
+
+
+
 
 // Simulationsgeschwindigkeit
 const double DT = 100.0;
@@ -120,8 +170,10 @@ class GameWindow : public Gosu::Window
 {
 public:
 	panzer p1;
+	rakete r1;
+
 	
-	GameWindow(): p1(300, 400, 0, 0), Window(800, 600)
+	GameWindow(): p1(300, 400, 0, 0), r1(-10, -10, 0, 0), Window(800, 600)
 	{
 		set_caption("Panzer");
 		
@@ -133,6 +185,11 @@ public:
 	void draw() override
 	{
 		p1.draw_panzer();
+		r1.draw_rakete();
+		
+		
+		
+
 
 	}
 
@@ -144,10 +201,19 @@ public:
 
 		//cout << p1.get_betrag() << endl;
 	
-		cout << p1.get_x()<<" "<<p1.get_y() << endl;
+		//cout << p1.get_x()<<" "<<p1.get_y() << endl;
 		hoch_runter(&p1);
 		rechts_links(&p1);
+		
+		
+		abschuss_move(&r1, &p1);
 		p1.move();
+		r1.move();
+		
+		
+		
+
+		
 		
 
 		
